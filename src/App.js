02bd071ec1411ecaf16493f9eb211e23da59314e
Konstanty Silwanowicz <kosto4ka1035@gmail.com>
2025-04-26 11:43:45 +0200

    12

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Products from './components/Products/Products.js';
+import Header from './components/Header/Header.js';
+import Container from './components/Container/Container.js';
+
+const App = () => {
+
+  return (
+    <Container>
+      <Header />
+      <Products />
+    </Container>
+  );
+}
+
+export default App;
