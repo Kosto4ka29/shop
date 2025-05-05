f40f8ffdc0297ab1b4f114673384e5e295eae2e1
Konstanty Silwanowicz <kosto4ka1035@gmail.com>
2025-05-05 12:59:57 +0200

    12

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,45 @@
+import PropTypes from 'prop-types';
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button.js';
 
-const Product = props => {
+const Product = ({ id, name, title, basePrice, colors, sizes }) => {
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+      <img
+  className={styles.image}
+  alt="Kodilla shirt"
+  src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--${currentColor}.jpg`} />
+
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map(size => (
+                <li key={size}>
+                  <button type="button">{size.name}</button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color => (
+                <li key={color}>
+                  <button 
+                    type="button" 
+                    className={clsx(styles[`color${color.charAt(0).toUpperCase() + color.slice(1)}`])}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -40,7 +48,16 @@ const Product = props => {
         </form>
       </div>
     </article>
-  )
+  );
+};
+
+Product.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  sizes: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Product;
